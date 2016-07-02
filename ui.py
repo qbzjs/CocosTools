7808eb564491c5d78df1343ff5124618ad84d783
张路欣 <117408802@qq.com>
2016-07-02 19:08:39 +0800

    添加ccs name的设置

--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
 import os
-
+import ConfigParser
 import log
 
 ROOT_DIR = "../"
@@ -7,9 +7,16 @@ OUT_DIR = "../res/"
 
 
 def _cocos_stdio_publish():
-    arg = "\"" + os.getenv('COCOS_ROOT') + "/Cocos.Tool" + "\"" + " publish -r -f " + os.path.abspath(
-        ROOT_DIR) + "/ma.ccs -o " + os.path.abspath(OUT_DIR) + " -d Serializer_Json"
-    os.system(arg)
+    cf = ConfigParser.ConfigParser()
+    cf.read("project.conf")
+    try:
+        ccs_name = cf.get("ccs", "ccs_name")
+    except ConfigParser.NoOptionError, e:
+        log.error(e)
+    else:
+        arg = "\"" + os.getenv('COCOS_ROOT') + "/Cocos.Tool" + "\"" + " publish -r -f " + os.path.abspath(
+            ROOT_DIR) + "/" + eval(ccs_name) + ".ccs -o " + os.path.abspath(OUT_DIR) + " -d Serializer_Json"
+        os.system(arg)
 
 
 def publish():
